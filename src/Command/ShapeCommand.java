0f88195322d44e54c88b752a9813aa6f51ae201b
LongTrac <let020@myutsa.edu>
2019-10-25 18:01:16 -0500

    Adding missing files

---
+++ src/Command/ShapeCommand.java
@@ -0,0 +1,50 @@
+package Command;
+
+import java.awt.Color;
+
+import Builder.CircleBuilder;
+import Builder.Shape;
+import Builder.ShapeBuilder;
+import Builder.ShapeEngineer;
+
+public abstract class ShapeCommand implements ShapeCommandInterface{
+	Shape shape;
+
+	public void create() {
+
+	}
+
+	public void draw() {
+		
+	}
+	
+	@Override
+	public void color(Color color) {
+		System.out.println("color");
+		if(Color.RED == color || Color.BLUE == color
+				|| Color.YELLOW == color || Color.ORANGE == color
+				|| Color.GREEN == color)
+			{
+				this.shape.setColor(color);
+			}
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void undo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void move(int x, int y) {
+		System.out.println("move");
+
+	}
+}
